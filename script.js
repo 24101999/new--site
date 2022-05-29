954967ddc410f704e9e7f48463bdffe21d259e7f
Henrique <henriquedasilvacosta@live.com>
2022-05-29 10:25:40 -0300

    add js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const inputForm = document.querySelectorAll('.input-form')
 const button = document.querySelector('.button')
 
-// button.addEventListener('click', (bt) => {
-//     inputForm.forEach((el) => {
-//         console.log(el)
-//         if (el.value == '') {
-//             bt.preventDefault()
-//             el.classList.add('active')
-//             el.classList.add('place')
-//         }
-//     })
-// })
+button.addEventListener('click', (bt) => {
+    inputForm.forEach((el) => {
+        console.log(el)
+        if (el.value == '') {
+            bt.preventDefault()
+            el.classList.add('active')
+            el.classList.add('place')
+        }
+    })
+})
